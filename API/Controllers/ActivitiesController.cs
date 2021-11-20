aaac94422d799718fab28a30241b965177278ae6
chama <chama@DESKTOP-VHA5BL5>
2021-11-21 00:09:37 +1100

    End of section 3

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,34 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            //return Mediator.Send(new List.Query());
+            return await _context.Activities.ToListAsync();
+
+        //var aa = new List<Activity>{new Activity()};
+           //return await aa.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+             return await _context.Activities.FindAsync(id);
+        }
+
+    }
+}
